613a8c636d1af44fa51f5c8edb49f775a8cff069
emil87ax <emil87ax@gmail.com>
2014-10-01 12:39:45 +0200

    ändrade språket

--- calculator.py
+++ calculator.py
@@ -1,34 +1,34 @@
-print("1: ADDITION")
-print("2: SUBTRACTION")
-print("3: MULTIPLICATION")
-print("4: DIVISION")
+print("1: Addera")
+print("2: Subtrahera")
+print("3: Multiplicera")
+print("4: Dividera")
 
-CHOICE = raw_input("Enter the Numbers:")
+CHOICE = raw_input("Skriv ett nummer:")
 
 if CHOICE == "1":
-    a = input("Enter the value of a:")
-    b = input("Enter the value of b:")
+    a = input("Skriv tal a:")
+    b = input("skriv tal b:")
     c = a + b
     print c
 
 elif CHOICE == "2":
-    a = input("Enter the value of a:")
-    b = input("Enter the value of b:")
+    a = input("Skriv tal a:")
+    b = input("Skriv tal b:")
     c = a - b
     print c
 
 elif CHOICE == "3":
-    a = input("Enter the value of a:")
-    b = input("Enter the value of b:")
+    a = input("Skriv tal a:")
+    b = input("Skriv tal b:")
     c = a * b
     print c
 
 elif CHOICE == "4":
-    a = input("Enter the value of a:")
-    b = input("Enter the value of b:")
+    a = input("Skriv tal a:")
+    b = input("Skriv tal b:")
     c = a / b
     print c
 
 else: 
- print "Invalid Number"
+ print "felaktigt nummer"
  print "\n"
